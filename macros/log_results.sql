8f9d6c405cba608d9e98416e6315d5261229af87
Anais Vaillant <anais.vaillant@fishtownanalytics.com>
2022-12-16 16:11:07 +0000

    testing log results

---
+++ macros/log_results.sql
@@ -0,0 +1,16 @@
+
+{% macro log_results(results) %}
+
+  {% if execute %}
+  {{ log("========== Begin Summary ==========", info=True) }}
+  {% for res in results -%}
+    {% set line -%}
+        node: {{ res.node.unique_id }}; status: {{ res.status }}))
+    {%- endset %}
+
+    {{ log(line, info=True) }}
+  {% endfor %}
+  {{ log("========== End Summary ==========", info=True) }}
+  {% endif %}
+
+{% endmacro %}
