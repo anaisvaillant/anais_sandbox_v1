372d54ad2b7e3687fe43ca7de0afd2e2e096be4a
Anais Vaillant <anais.vaillant@fishtownanalytics.com>
2023-07-27 05:02:16 +0000

    adding in ML formula1

--- models/staging/formula1/stg_f1_circuits.sql
+++ models/staging/formula1/stg_f1_circuits.sql
@@ -1,21 +1,18 @@
 with
-
-    circuits as (
-        select * from {{ source('formula1', 'circuits') }}
-    ),
-
+    source as (select * from {{ source('formula1', 'circuits') }}),
+    
     renamed as (
         select
             circuit_id as circuit_id,
             circuit_ref as circuit_ref,
             name as circuit_name,
-            location as circuit_location,
-            country as circuit_country,
+            location as location,
+            country as country,
             lat as latitude,
             lng as longitude,
             to_number(iff(contains(alt, 'N'), null, alt)) as altitude,
             url as circuit_url
-        from circuits
+        from source
     )
-
-select * from renamed
+select *
+from renamed
---
+++ models/staging/formula1/stg_f1_status.sql
@@ -0,0 +1,12 @@
+with
+
+source  as (select * from {{ source('formula1','status') }}),
+
+renamed as (
+    select
+        status_id as status_id,
+        status
+    from source
+)
+
+select * from renamed
